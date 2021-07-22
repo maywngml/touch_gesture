7961b9ad13199d21a544c712aee66b49a23cd28f
maywngml <maywngml@naver.com>
2021-07-22 10:35:16 +0900

    [Update] 라이브러리 사용하지 않고 핀치 줌 인/아웃 직접 구현

---
+++ src/Contents.js
@@ -0,0 +1,141 @@
+import golf from "./assets/golf.jpg";
+import sampleVideo from "./assets/sample_video.mp4";
+import shot from "./assets/shot.png";
+import sampleImg from "./assets/sample_img.jpg";
+
+const contents = [
+    {
+        id: 1,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 2,
+        src: sampleVideo,
+        bigSrc: sampleVideo,
+        isVideo: true,
+    },
+    {
+        id: 3,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 4,
+        src: shot,
+        bigSrc: shot,
+        isVideo: false,
+    },
+    {
+        id: 5,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 6,
+        src: sampleVideo,
+        bigSrc: sampleVideo,
+        isVideo: true,
+    },
+    {
+        id: 7,
+        src: shot,
+        bigSrc: shot,
+        isVideo: false,
+    },
+    {
+        id: 8,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 9,
+        src: sampleVideo,
+        bigSrc: sampleVideo,
+        isVideo: true,
+    },
+    {
+        id: 10,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 11,
+        src: shot,
+        bigSrc: shot,
+        isVideo: false,
+    },
+    {
+        id: 12,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 13,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 14,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 15,
+        src: shot,
+        bigSrc: shot,
+        isVideo: false,
+    },
+    {
+        id: 16,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 17,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 18,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 19,
+        src: shot,
+        bigSrc: shot,
+        isVideo: false,
+    },
+    {
+        id: 20,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 21,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+    {
+        id: 22,
+        src: golf,
+        bigSrc: golf,
+        isVideo: false,
+    },
+];
+
+export default contents;
--- src/Image.Grid.js
+++ src/Image.Grid.js
@@ -1,42 +1,64 @@
 import React, { useState, useRef } from "react";
 import PhotoGrid from "react-photo-feed";
-import QuickPinchZoom from "react-quick-pinch-zoom";
+import { useGesture, usePinch } from "react-use-gesture";
+import Contents from "./Contents";
+// import QuickPinchZoom from "react-quick-pinch-zoom";
 // import Hammer from "react-hammerjs";
 import "./Image.Grid.css";
+import playButton from "./assets/playButton.png";
+import "react-photo-feed/library/style.css";
+
 import golf from "./assets/golf.jpg";
 import sampleVideo from "./assets/sample_video.mp4";
 import shot from "./assets/shot.png";
-import playButton from "./assets/playButton.png";
-import sampleImg from "./assets/sample_img.jpg";
-import "react-photo-feed/library/style.css";
 
 function ImageGrid() {
-    const [cardWidthIndex, setCardWidthIndex] = useState(0);
+    const [cardWidthIndex, setCardWidthIndex] = useState(2);
     const [pinchAble, setPinchAble] = useState(true);
+    // const [scrollAble, setScrollAble] = useState(true);
     const [pinchFlag, setPinchFlag] = useState({
         pinchInFlag: false,
         pinchOutFlag: false,
     });
     const { pinchInFlag, pinchOutFlag } = pinchFlag;
+    const [distance, setDistance] = useState(0);
     const cardWidthArray = [1, 2, 3, 4, 6];
     let pointerCache = [];
+    let prevDistance = -1,
+        pinchOutStartDistance = -1,
+        pinchInStartDistance = -1;
+    let pinchChangeFlag = false;
+
+    const calDistance = (prev, cur) => {
+        const distanceX =
+            Math.abs(prev.clientX - cur.clientX) *
+            Math.abs(prev.clientX - cur.clientX);
+        const distanceY =
+            Math.abs(prev.clientY - cur.clientY) *
+            Math.abs(prev.clientY - cur.clientY);
+        return Math.sqrt(distanceX + distanceY);
+    };
 
     const handlePinchIn = () => {
         if (!pinchInFlag && cardWidthIndex < 4) {
-            setPinchFlag({ pinchInFlag: true, pinchOutFlag: false });
+            setPinchFlag({
+                pinchInFlag: true,
+                pinchOutFlag: false,
+            });
+            pinchChangeFlag = false;
             setCardWidthIndex(cardWidthIndex + 1);
         }
     };
 
     const handlePinchOut = () => {
         if (!pinchOutFlag && cardWidthIndex > 0) {
-            setPinchFlag({ pinchInFlag: false, pinchOutFlag: true });
+            setPinchFlag({
+                pinchInFlag: false,
+                pinchOutFlag: true,
+            });
+            pinchChangeFlag = false;
             setCardWidthIndex(cardWidthIndex - 1);
         }
-    };
-
-    const handlePinchEnd = () => {
-        setPinchFlag({ pinchInFlag: false, pinchOutFlag: false });
     };
 
     const removeEvent = (e) => {
@@ -52,9 +74,11 @@ function ImageGrid() {
 
     const handlePointerDown = (e) => {
         pointerCache.push(e);
-        if (pointerCache.length >= 2) {
-            setPinchAble(true);
-        }
+        // pointerCache가 1개일때는 div touch-action: none 아님,
+        // none 부분을 state값으로 주기
+        // if (pointerCache.length >= 2) {
+        //     setPinchAble(true);
+        // }
     };
 
     const handlePointerMove = (e) => {
@@ -62,182 +86,79 @@ function ImageGrid() {
             (cache) => cache.pointerId === e.pointerId,
         );
         pointerCache[index] = e;
+        if (pointerCache.length === 2) {
+            const curDistance = calDistance(pointerCache[0], pointerCache[1]);
+            // changeOptionsPinch(true);
+            // setDistance(curDistance);
+            if (prevDistance === -1) {
+                pinchOutStartDistance = curDistance;
+                pinchInStartDistance = curDistance;
+            } else {
+                if (!pinchChangeFlag) {
+                    if (pinchOutFlag && curDistance < prevDistance) {
+                        pinchInStartDistance = prevDistance;
+                        pinchChangeFlag = true;
+                    }
+                    if (pinchInFlag && curDistance > prevDistance) {
+                        pinchOutStartDistance = prevDistance;
+                        pinchChangeFlag = true;
+                    }
+                }
+                if (curDistance > pinchOutStartDistance + 1) {
+                    handlePinchOut();
+                }
+                if (curDistance < pinchInStartDistance - 1) {
+                    handlePinchIn();
+                }
+            }
+            prevDistance = curDistance;
+        }
     };
 
     const handlePointerUp = (e) => {
         removeEvent(e);
         if (pointerCache.length < 2) {
-            setPinchAble(false);
+            prevDistance = -1;
+            pinchOutStartDistance = -1;
+            pinchInStartDistance = -1;
+            setPinchFlag({
+                pinchInFlag: false,
+                pinchOutFlag: false,
+            });
+            pinchChangeFlag = false;
+            // changeOptionsPinch(false);
         }
     };
 
-    const contents = [
-        {
-            id: 1,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 2,
-            src: sampleVideo,
-            bigSrc: sampleVideo,
-            isVideo: true,
-        },
-        {
-            id: 3,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 4,
-            src: shot,
-            bigSrc: shot,
-            isVideo: false,
-        },
-        {
-            id: 5,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 6,
-            src: sampleVideo,
-            bigSrc: sampleVideo,
-            isVideo: true,
-        },
-        {
-            id: 7,
-            src: shot,
-            bigSrc: shot,
-            isVideo: false,
-        },
-        {
-            id: 8,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 9,
-            src: sampleVideo,
-            bigSrc: sampleVideo,
-            isVideo: true,
-        },
-        {
-            id: 10,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 11,
-            src: shot,
-            bigSrc: shot,
-            isVideo: false,
-        },
-        {
-            id: 12,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 13,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 14,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 15,
-            src: shot,
-            bigSrc: shot,
-            isVideo: false,
-        },
-        {
-            id: 16,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 17,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 18,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 19,
-            src: shot,
-            bigSrc: shot,
-            isVideo: false,
-        },
-        {
-            id: 20,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 21,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-        {
-            id: 22,
-            src: golf,
-            bigSrc: golf,
-            isVideo: false,
-        },
-    ];
-
     return (
-        <QuickPinchZoom
-            onZoomUpdate={handlePinchIn}
-            onZoomEnd={handlePinchEnd}
-            // onPinchOut={handlePinchOut}
-            // onPinchEnd={handlePinchEnd}
-            // options={{
-            //     recognizers: {
-            //         pinch: { enable: pinchAble },
-            //     },
-            // }}
-        >
+        <>
+            <div>
+                version 2, {pinchAble ? 1 : 0}, pinchInFlag:
+                {pinchInFlag ? 1 : 0}, pinchOutFlag: {pinchOutFlag ? 1 : 0},
+                distance: {distance}
+            </div>
             <div
                 className="touch-area"
-                // onPointerDown={handlePointerDown}
-                // onPointerMove={handlePointerMove}
-                // onPointerUp={handlePointerUp}
-                // onPointerCancel={handlePointerUp}
-                // onPointerOut={handlePointerUp}
-                // onPointerLeave={handlePointerUp}
+                // ref={touchTarget}
+                onPointerDown={pinchAble && handlePointerDown}
+                onPointerMove={pinchAble && handlePointerMove}
+                onPointerUp={handlePointerUp}
+                onPointerCancel={handlePointerUp}
+                onPointerOut={handlePointerUp}
+                onPointerLeave={handlePointerUp}
+                // style={{ touchAction: pinchAble ? "none" : "auto" }}
+                style={{ touchAction: "none" }}
             >
-                version 58, {pinchAble ? 1 : 0}
                 <PhotoGrid
                     pinchInFlag={pinchInFlag}
                     pinchOutFlag={pinchOutFlag}
                     columns={cardWidthArray[cardWidthIndex]}
-                    photos={contents}
+                    photos={Contents}
                     playButton={playButton}
                     changeOptionsPinch={changeOptionsPinch}
                 />
             </div>
-        </QuickPinchZoom>
+        </>
     );
 }
 
